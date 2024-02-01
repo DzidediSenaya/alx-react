e69c321d8dfabcc98e59ba82a673f7ab618bc455
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-01 01:22:05 +0000

    Update index.js

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,11 +1,20 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Import your main React component
+import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+render();
+
+// Enable Hot Module Replacement (HMR)
+if (module.hot) {
+  module.hot.accept('./App', render);
+}
