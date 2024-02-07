e8bd1df335a57b234798ec1c2eae199edbde6a0b
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-07 21:54:49 +0000

    Update App.js

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -1,34 +1,55 @@
 // App.js
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 
-const App = ({ isLoggedIn }) => {
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
+class App extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
 
-  return (
-    <div>
-      {isLoggedIn ? (
-        <CourseList listCourses={listCourses} />
-      ) : (
-        <Login />
-      )}
-    </div>
-  );
-};
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    const { logOut } = this.props;
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      logOut();
+    }
+  };
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+
+    return (
+      <div>
+        {isLoggedIn ? (
+          <CourseList listCourses={listCourses} />
+        ) : (
+          <Login />
+        )}
+      </div>
+    );
+  }
+}
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func.isRequired,
 };
 
 App.defaultProps = {
   isLoggedIn: false,
+  logOut: () => {}, // Empty function as default value
 };
 
 export default App;
-
