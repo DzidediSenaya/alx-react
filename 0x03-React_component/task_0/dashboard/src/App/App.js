5200100cf9719175903dc14350965e12c9f7c270
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-07 21:48:43 +0000

    Update App.js

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,26 +1,30 @@
 // App.js
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 
-const App = ({ isLoggedIn }) => {
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
+class App extends Component {
+  render() {
+    const { isLoggedIn } = this.props;
 
-  return (
-    <div>
-      {isLoggedIn ? (
-        <CourseList listCourses={listCourses} />
-      ) : (
-        <Login />
-      )}
-    </div>
-  );
-};
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+
+    return (
+      <div>
+        {isLoggedIn ? (
+          <CourseList listCourses={listCourses} />
+        ) : (
+          <Login />
+        )}
+      </div>
+    );
+  }
+}
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
