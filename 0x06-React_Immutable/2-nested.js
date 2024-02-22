f95dd4c97b243d911a03ac113672783638a7516f
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-22 21:13:49 +0000

    2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,14 +1,5 @@
-/**
- * Accesses a value in an immutable object at the specified path.
- * @param {Object} object - The plain JavaScript object to access.
- * @param {Array} array - An array containing a list of keys representing the path to the desired value.
- * @returns {any|undefined} - The value at the defined path, or undefined if the path is not valid.
- */
-import { fromJS } from 'immutable';
+import { fromJS, getIn } from 'immutable';
 
-const accessImmutableObject = (object, array) => {
-  const mappedObj = fromJS(object);
-  return mappedObj.getIn(array, undefined);
-};
-
-export default accessImmutableObject;
+export default function accessImmutableObject(object, array) {
+  return getIn(fromJS(object), array, undefined);
+}
