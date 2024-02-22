cfde28febbd6b7efc5d07eea54645beecfc4e62f
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-22 21:04:15 +0000

    Update 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,23 +1,21 @@
 import { Seq } from 'immutable';
 
-/**
- * Filters students with a score less than 70 and prints their first name and last name
- * with the first letter capitalized.
- * @param {Object} grades - The object containing student grades.
- */
-export function printBestStudents(grades) {
-    Seq(grades)
-        // Filter students with a score less than 70
-        .filter(student => student.score >= 70)
-        // Map each student to a new object with capitalized first name and last name
-        .map(student => ({
-            ...student,
-            firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
-            lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1)
-        }))
-        // Convert the Seq back to a plain JavaScript object
-        .toObject()
-        // Print the resulting object to the console
-        .forEach(student => console.log(student));
-}
+const printBestStudents = (object) => {
+  const seq = Seq(object);
 
+  const getStudent = seq.filter((value) => value.score > 70);
+
+  const student = getStudent.toJS();
+
+  const formatName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+
+  Object.keys(student).map((key) => {
+    student[key].firstName = formatName(student[key].firstName);
+    student[key].lastName = formatName(student[key].lastName);
+    return student[key];
+  });
+
+  console.log(student);
+};
+
+export default printBestStudents;
