64725ec1b7b8e52e5db1ed7cf5b7f4d5223dfd62
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-22 21:12:01 +0000

    Update 6-deeply.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,14 +1,7 @@
 import { Map } from 'immutable';
 
-/**
- * Merges two objects deeply and returns a Map containing the merged values of both objects.
- * @param {Object} obj1 - The first object.
- * @param {Object} obj2 - The second object.
- * @returns {Map} - A Map containing the merged values of both objects.
- */
-function mergeDeeplyElements(obj1, obj2) {
-    // Use Immutable.js mergeDeep function to merge the objects deeply
-    return Map(obj1).mergeDeep(Map(obj2));
+function mergeDeeplyElements(page1, page2) {
+  return Map(page1).mergeDeep(Map(page2));
 }
 
 export default mergeDeeplyElements;
