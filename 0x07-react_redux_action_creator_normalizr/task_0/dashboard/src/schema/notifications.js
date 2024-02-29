32f92e6b73c0a5885fd950d3266eb9205ac38062
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-29 07:07:56 +0000

    Delete 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema directory

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,6 +0,0 @@
-import notificationsData from '../../notifications.json';
-
-export function getAllNotificationsByUser(userId) {
-  return notificationsData.filter(notification => notification.author.id === userId);
-}
-
