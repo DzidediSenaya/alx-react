a3c45c726b5c3c6b6d8991a4853b4bae6405b45c
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-29 07:09:56 +0000

    Delete 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/schema directory

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/schema/courseActionCreators.js
+++
@@ -1,9 +0,0 @@
-import { selectCourse, unSelectCourse } from './courseActionCreators';
-
-export const boundSelectCourse = index => dispatch => {
-  dispatch(selectCourse(index));
-};
-
-export const boundUnSelectCourse = index => dispatch => {
-  dispatch(unSelectCourse(index));
-};
